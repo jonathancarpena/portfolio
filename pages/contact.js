7c5277a2b2bb59b9603e64e23361878f89a5e4e0
Jonathan <jonathancarpena99@gmail.com>
2022-10-05 17:45:48 -0700

    Added form success page

---
+++ pages/contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FiHome } from 'react-icons/fi'
+import Link from 'next/link'
+
+function Contact() {
+    return (
+        <div className=' relative top-12 h-[70vh] flex-col flex justify-center items-center'>
+            <h1 className='text-center mb-5 text-5xl font-bold md:text-8xl'>
+                Message sent!
+            </h1>
+            <h2 className='text-4xl mb-10 '>
+                {`I'll get back to you in 24-48 hours.`}
+            </h2>
+
+
+            <Link href="/" >
+                <button className='rounded-xl active:scale-90 active:bg-accent-600 hover:scale-110 hover:bg-accent-500 transition-all ease-in-out duration-200 bg-black text-white px-10 py-4 flex  text-2xl space-x-1 items-center'>
+                    <FiHome />
+                    <span>Go Home</span>
+                </button>
+            </Link>
+
+
+        </div>
+    )
+}
+
+export default Contact
